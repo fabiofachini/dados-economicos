fcc2675f96225b9f739ce9903303f7d553d12f7f
Fabio Fachini <fabiofachini92@gmail.com>
2024-08-04 17:50:27 -0300

    atualização dos conteiners

--- executor.py
+++ executor.py
@@ -2,6 +2,7 @@ import pyodbc
 import time
 import subprocess
 import os
+import requests
 from dotenv import load_dotenv
 
 # Carregar as variáveis de ambiente do arquivo .env
@@ -55,6 +56,33 @@ def executar_scripts():
     except subprocess.CalledProcessError as e:
         print(f"Erro ao executar o script: {e}")
 
+def acessar_paginas_streamlit():
+    urls = [
+        "http://20.14.251.70:8501/PIB",
+        "http://20.14.251.70:8501/População",
+        "http://20.14.251.70:8501/Desemprego",
+        "http://20.14.251.70:8501/Desigualdade",
+        "http://20.14.251.70:8501/Renda",
+        "http://20.14.251.70:8501/Inflação",
+        "http://20.14.251.70:8501/Juros",
+        "http://20.14.251.70:8501/Crédito",
+        "http://20.14.251.70:8501/Câmbio",
+        "http://20.14.251.70:8501/Educação",
+        "http://20.14.251.70:8501/Confiança",
+        "http://20.14.251.70:8501/Energia"
+    ]
+    
+    for url in urls:
+        try:
+            response = requests.get(url)
+            time.sleep(5)
+            if response.status_code == 200:
+                print(f"Página acessada com sucesso: {url}")
+            else:
+                print(f"Falha ao acessar a página {url}: {response.status_code}")
+        except requests.exceptions.RequestException as e:
+            print(f"Erro ao acessar a página {url}: {e}")
+
 # Chamando a função para executar os scripts
 if __name__ == "__main__":
     # Estabelece a conexão com o banco de dados
@@ -62,5 +90,8 @@ if __name__ == "__main__":
     
     # Executa os scripts de ETL
     executar_scripts()
+
+    # Acessa as páginas do Streamlit para carregar o cache
+    acessar_paginas_streamlit()
 
     print("Extração, carregamento e transformação dos dados do BACEN e do IBGE finalizados.")
